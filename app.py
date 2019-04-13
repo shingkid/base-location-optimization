a4f0b93be69804f5e1d282676a959af45807c170
Jane Seah <janeseah@Janes-MacBook.lan>
2019-04-13 23:26:02 +0800

    Test upload zip file function

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
 import os
+import zipfile
+from flask import Flask, request, redirect, url_for, flash, render_template
+from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
+ALLOWED_EXTENSIONS = set(['zip'])
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-@app.route("/", methods=['POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    pass
-    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            zip_ref = zipfile.ZipFile(os.path.join(UPLOAD_FOLDER, filename), 'r')
+            zip_ref.extractall(UPLOAD_FOLDER)
+            zip_ref.close()
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
